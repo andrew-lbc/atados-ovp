7d900c15bce063fb55e2b50ea29ab2b1171ed2c8
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-01 06:05:23 -0300

    Add django-git-submodules and coverage to requirements.txt

--- server/settings.py
+++ server/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Submodules config
+from dj_git_submodule import submodule
+submodule.add('django-ovp-users')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ovp_users',
 ]
 
 MIDDLEWARE = [
