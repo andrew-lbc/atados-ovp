16240bf59068b60d4422d541573dc74bf10d6d9e
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-01 21:52:15 -0300

    Add rest_framework and rest_framework_swagger to settings.py

--- server/settings.py
+++ server/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_swagger',
     'ovp_users',
 ]
 
