769d69ae611c086d35958b2d56aaac657855d161
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-13 18:11:01 -0300

    Upgrade dj-git-submodules to v0.1.7 and load local submodules with submodule.locate

--- server/settings.py
+++ server/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from dj_git_submodule import submodule
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Submodules config
-from dj_git_submodule import submodule
-submodule.add('django-ovp-users')
+submodule.add(submodule.locate('django-*'))
 
 
 # Quick-start development settings - unsuitable for production
