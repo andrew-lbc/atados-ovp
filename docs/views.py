a8587fb0ea7ed9604f31ee723e0d1909d1a3bb1d
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-14 21:12:36 -0300

    Update SchemaGenerator to OVPSchemaGenerator

--- docs/views.py
+++ docs/views.py
@@ -1,11 +1,12 @@
-from rest_framework import schemas
 from rest_framework import decorators
 from rest_framework import response
 from rest_framework_swagger import renderers
 
+from ovp_core import schemas
+
 @decorators.api_view()
 @decorators.renderer_classes([renderers.OpenAPIRenderer, renderers.SwaggerUIRenderer])
 def schema_view(request):
-  generator = schemas.SchemaGenerator(title='Atados API')
+  generator = schemas.OVPSchemaGenerator(title='Atados API')
   schema = generator.get_schema(request=request)
   return response.Response(schema)
