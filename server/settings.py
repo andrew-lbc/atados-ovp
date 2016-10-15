6b3092737ae3a834750e84d5e9ecc813c13c7730
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-14 22:50:50 -0300

    Disable browsable API

--- server/settings.py
+++ server/settings.py
@@ -122,6 +122,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
     )
 }
 
