4ce4ad2e5178e658fef796c83e17502352553adb
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-10-11 18:26:03 -0300

    Remove aws info from production settings

--- api/server/production.py
+++ api/server/production.py
@@ -12,16 +12,6 @@ EMAIL_BACKEND = 'email_log.backends.EmailBackend'
 # Media and static files
 MEDIA_ROOT= '/tmp'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-AWS_S3_SECURE_URLS = True
-AWS_S3_URL_PROTOCOL = 'https'
-AWS_HEADERS = {
-    'Expires': 'Sat, 31 Dec 2016 23:59:59 GMT'
-}
-
 HAYSTACK_CONNECTIONS = {
   'default': {
     'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
