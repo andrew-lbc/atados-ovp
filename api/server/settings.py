27412dac394728b21ae4e232edd7c9b497404c1e
Tais <tais.alemar@gmail.com>
2018-03-29 16:10:47 -0300

    update submodule / import-export

--- api/server/settings.py
+++ api/server/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = get_core_apps() + [
     'oauth2_provider',
     'social_django',
     'rest_framework_social_oauth2',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -106,14 +107,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
