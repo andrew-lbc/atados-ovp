e5d6bdb94d8224a0d5d951f8d3ad01d7d4babecf
Leonardo Arroyo <contato@leonardoarroyo.com>
2018-07-19 17:53:14 -0300

    Update celery configs

--- api/server/server.sh
+++ api/server/server.sh
@@ -2,4 +2,4 @@
 
 PRJ=`pwd | rev | cut -d "/" -f3 | rev`
 gunicorn server.wsgi:application -w 5 --timeout 300 --limit-request-line 16382 --bind unix:/tmp/api.$PRJ.socket
-#celery -A atados worker --app=atados.celery_app:app --loglevel=info
+cd .. && celery -A server worker --loglevel=warning
