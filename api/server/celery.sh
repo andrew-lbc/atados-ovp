64d479d20086e694728416d2519f2d50e399dd3c
Leonardo Arroyo <contato@leonardoarroyo.com>
2018-07-19 19:08:11 -0300

    Remove unused file

--- api/server/celery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ..
-celery multi stopwait worker1 --pidfile="/tmp/celery.%n.pid"
-celery multi start worker1 -A server \
-    --logfile="$HOME/api/logs/celery/%n%I.log" \
-    --pidfile="/tmp/celery.%n.pid"
