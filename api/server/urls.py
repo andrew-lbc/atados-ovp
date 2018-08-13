71768fb0fd145c771ae4e4b591b6c89b307656d4
Leonardo Arroyo <contato@leonardoarroyo.com>
2018-08-13 13:33:48 -0300

    Add drf-yasg

--- api/server/urls.py
+++ api/server/urls.py
@@ -16,10 +16,27 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 import sandbox.urls
 import docs.urls
 import channels.pv.urls
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   validators=['flex', 'ssv'],
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # API Documentation
@@ -30,6 +47,10 @@ urlpatterns = [
 
     # PV Specific endpoints
     url(r'^pv-channel/', include(channels.pv.urls)),
+
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
