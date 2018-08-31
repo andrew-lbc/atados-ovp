e3d2616d02df29799d4ff0cf5ae85c806f3f3d8b
Fabio Garcia <fabioo.salgado@gmail.com>
2018-08-31 15:43:03 -0300

    Fix pagination class

--- api/server/settings.py
+++ api/server/settings.py
@@ -136,7 +136,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Rest framework
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'ovp.apps.search.pagination.StandardResultsSetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'ovp.apps.core.pagination.StandardResultsSetPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
