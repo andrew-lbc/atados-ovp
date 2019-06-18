e49c253478fd9d9b3f1ccccaa25864b6fb457c28
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-06-18 10:25:26 -0300

    Add @boehringer.com to email validation

--- api/channels/boehringer/validators.py
+++ api/channels/boehringer/validators.py
@@ -1,5 +1,5 @@
 def boehringer_user_email_validator(data, context):
   if context['request'].channel == 'boehringer':
     email = data.get('email', '')
-    if not email.endswith('@boehringer-ingelheim.com'):
+    if not email.endswith('@boehringer-ingelheim.com') or not email.endswith('@boehringer.com'):
       return {'email': ['Email address must be from @boehringer-ingelheim.com']}
